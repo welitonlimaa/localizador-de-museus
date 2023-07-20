9d2045b8983453c9ac180abbe79d84bd1a94f0d3
welitonlimaa <welitonlima159@gmail.com>
2023-07-20 15:29:31 -0300

    req 8

---
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
@@ -0,0 +1,29 @@
+package com.betrybe.museumfinder.solution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.dto.CollectionTypeCount;
+import com.betrybe.museumfinder.service.CollectionTypeService;
+import org.junit.jupiter.api.Test;
+
+class CollectionTypeServiceTest {
+
+  @Test
+  void testCountByCollectionTypes() {
+    MuseumFakeDatabase databaseMock = mock(MuseumFakeDatabase.class);
+    CollectionTypeService service = new CollectionTypeService(databaseMock);
+
+    String typesList = "history,image";
+    when(databaseMock.countByCollectionType("history")).thenReturn(14L);
+    when(databaseMock.countByCollectionType("image")).thenReturn(42L);
+
+    CollectionTypeCount result = service.countByCollectionTypes(typesList);
+
+    assertEquals(2, result.collectionTypes().length);
+    assertEquals(14L, result.count());
+    assertEquals(42L, result.count());
+  }
+}
